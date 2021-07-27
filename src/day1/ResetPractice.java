4547f8a0da65aa80f004b66432cc161f94e59b55
kadir211 <yohansmillion@yahoo.com>
2021-07-26 20:04:34 -0700

    Test case all done

--- src/day1/ResetPractice.java
+++ src/day1/ResetPractice.java
@@ -5,5 +5,10 @@ public class ResetPractice {
     // part 1 is done
     // part 2 is done
     // part 3 is done
+    // part 4 is done
+    // part 5 is done
+    //Mixed|soft rest remove tek commit from history but keep the local changes
+    // Did part 4 and 5 and I am done in one commit
+
 
 }
